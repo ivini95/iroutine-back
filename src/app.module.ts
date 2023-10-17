94df786c5a63b120498df0fedd518fd591c30518
ivini95 <viniximenesdev@gmail.com>
2023-10-17 18:06:44 -0300

    feat: export user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TaskModule, PrismaModule],
+  imports: [TaskModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
