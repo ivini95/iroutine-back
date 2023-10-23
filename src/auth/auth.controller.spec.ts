a1aeac2925c3d655899bc72fcf12458416eeaa04
ivini95 <viniximenesdev@gmail.com>
2023-10-23 17:07:50 -0300

    test: testing login

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,8 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+const fakeToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTMyZjU0ZTYzOWI2ZWExYjAwNjJlMTgiLCJpYXQiOjE2OTgwODEyNzksImV4cCI6MTY5ODE2NzY3OX0.HMwmA_vWpE_9XCKuCs8XWFepyeKIvn-ag323cQjq-bA';
+
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +14,28 @@ describe('AuthController', () => {
       providers: [
         {
           provide: AuthService,
-          useValue: {},
+          useValue: {
+            login: jest.fn().mockResolvedValue(fakeToken),
+          },
         },
       ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should login account', async () => {
+      const response = await authController.login({
+        name: 'fakeName',
+        password: 'fakepassword',
+      });
+
+      expect(response).toEqual(fakeToken);
+    });
   });
 });
